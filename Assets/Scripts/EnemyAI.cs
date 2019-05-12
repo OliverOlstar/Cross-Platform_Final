9e76ab40ed8bfe26458b38e6c0c6b0b978f7e1b8
Oliver <Olstar_123@users.noreply.github.com>
2019-05-12 19:16:13 -0400

    First Commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    bool agro = false; 
+    bool alive = true; 
+    public GameObject player;
+    public Transform model;
+    Animator anim;
+    public float runSpeed = 7;
+    public float walkSpeed = 4;
+    public float acel = 4;
+
+    void Start()
+    {
+        anim = model.GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance <= 4.5f)
+        {
+            if (distance >= 1.5f)
+            {
+                anim.ResetTrigger("Attack");
+                anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), walkSpeed, acel * Time.deltaTime));
+                agro = true;
+            }
+            else
+            {
+                anim.SetTrigger("Attack");
+                anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), 0, acel * Time.deltaTime));
+                agro = false;
+            }
+        } else if (distance <= 10)
+        {
+            anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), runSpeed, acel * Time.deltaTime));
+            agro = true;
+        }
+
+        if (!alive)
+        {
+            anim.SetTrigger("Dead");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            alive = false;
+        }
+
+        if (agro && alive)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), anim.GetFloat("Speed") * Time.deltaTime);
+            model.transform.LookAt(new Vector3(player.transform.position.x, model.transform.position.y, player.transform.position.z));
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float walkSpeed;
+    public float runSpeed;
+    public float jumpForce;
+    Rigidbody rb;
+    public Animator anim;
+    public Transform model;
+    public bool alive = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alive)
+        {
+            anim.SetTrigger("Death");
+        } else
+        {
+            Move();
+            Jump();
+            Shoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            alive = false;
+        }
+    }
+
+    void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal");
+        float deltaZ = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(deltaX, 0, deltaZ);
+
+        moveVector.Normalize();
+        moveVector = Camera.main.transform.TransformDirection(moveVector);
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), moveVector.magnitude * runSpeed, 0.1f));
+            moveVector *= Time.deltaTime * runSpeed;
+        }
+        else
+        {
+            anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), moveVector.magnitude * walkSpeed, 0.1f));
+            moveVector *= Time.deltaTime * walkSpeed;
+        }
+
+        transform.Translate(moveVector.x, 0, moveVector.z);
+        model.transform.LookAt(new Vector3(moveVector.x + transform.position.x, model.transform.position.y, moveVector.z + transform.position.z));
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            anim.SetTrigger("Jump");
+        }
+    }
+
+    void Shoot()
+    {
+        anim.ResetTrigger("Shoot");
+        anim.ResetTrigger("Shoot2");
+        anim.ResetTrigger("Spell");
+
+        if (Input.GetButtonDown("Fire1"))
+            anim.SetTrigger("Shoot");
+
+        if (Input.GetButtonDown("Fire2"))
+            anim.SetTrigger("Shoot2");
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            anim.SetTrigger("Spell");
+    }
+}
