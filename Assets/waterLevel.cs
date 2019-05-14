eb23acd9e416dcf85285a04ddfc36eb6b5084e7b
Oliver <Olstar_123@users.noreply.github.com>
2019-05-13 21:52:39 -0400

    The first level I made

---
+++ Assets/waterLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterLevel : MonoBehaviour
+{
+    public float[] areaY;
+    public int targetArea = 0;
+    public float speed = 0.5f;
+    
+    void Update()
+    {
+        if (transform.position.y + areaY[targetArea] > speed)
+        {
+            transform.position = new Vector3(0, transform.position.y - speed);
+        }
+        else if (transform.position.y + areaY[targetArea] < -speed)
+        {
+            transform.position = new Vector3(0, transform.position.y + speed);
+        }
+    }
+}
